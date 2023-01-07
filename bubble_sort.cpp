13584431cd1cc34871600cf1a5de6a7141e3fb00
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-01-07 18:05:30 +0900

    first commit

---
+++ bubble_sort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void bubble_sort(int list[], int n)
+{
+    for(int i=n-1;i>0;i--)
+    {
+        for(int j=0;j<i;j++)
+        {
+            if(list[j]<list[j+1])
+            {
+                swap(list[j],list[j+1]);
+            }
+        }
+    }
+}
